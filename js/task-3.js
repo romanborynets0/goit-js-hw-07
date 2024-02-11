0be86a263c920f1fd964faba40a1b128bac05bc8
romanborynets0 <romanborynets@gmail.com>
2024-02-11 11:45:57 +0200

    fixed problem with 3 task

--- js/task-3.js
+++ js/task-3.js
@@ -2,5 +2,6 @@ const textInput = document.getElementById("name-input");
 const output = document.getElementById("name-output");
 
 textInput.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value != "" ? event.currentTarget.value : "Anonymous";
+  const trimmedValue = event.currentTarget.value.trim();
+  output.textContent = trimmedValue !== "" ? trimmedValue : "Anonymous";
 });
